534dd7a1200899932abacb3e4a808957f75c6488
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-09-15 21:18:09 +0530

    cc

--- screens/Homescreens.js
+++ screens/Homescreens.js
@@ -1,46 +1,42 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {View, KeyboardAvoidingView } from "react-native";
 import io from "socket.io-client";
+import { GiftedChat } from "react-native-gifted-chat";
+
 
 export default function HomeScreen() {
-  const [messageToSend, setMessageToSend] = useState("");
+ 
   const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://192.168.43.193:3001");
     socket.current.on("message", message => {
-      setRecvMessages(prevState => [...prevState, message]);
+      debugger;
+      console.log("message", message);
+      setRecvMessages(prevState => GiftedChat.append(prevState, message));
     });
   }, []);
 
-  const sendMessage = () => {
-    socket.current.emit("message", messageToSend);
-    setMessageToSend("");
+  const onSend = messages => {
+    console.log(messages);
+    socket.current.emit("message", messages[0].text);
+    setRecvMessages(prevState => GiftedChat.append(prevState, messages));
   };
 
-  const textOfRecvMessages = recvMessages.map(msg => (
-    <Text key={msg}>{msg}</Text>
-  ));
+
 
   return (
-    <View style={styles.container}>
-      {textOfRecvMessages}
-      <TextInput
-        value={messageToSend}
-        onChangeText={ text => setMessageToSend(text)}
-        placeholder="Enter chat messsage.."
-        onSubmitEditing={sendMessage}
+    <View style={{ flex: 1 }}>
+      <GiftedChat
+        messages={recvMessages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
       />
+      {Platform.OS === "android" && <KeyboardAvoidingView behavior="padding" />}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
